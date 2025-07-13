27ddaaf15710737d2407895ca44d7954c079891e
r-valero <r.valero0600@gmail.com>
2025-07-13 14:55:02 +0200

    priemros ejercicios

---
+++ ejercicio1.js
@@ -0,0 +1,28 @@
+/* Ejercicio 1
+1. Define tu superhéroe favorito:
+Crea una variable llamada miSuperheroeFavorito y asigna el valor "Hulk".
+2. Define un número:
+Crea una variable llamada numeroFavorito y asigna el valor 50.
+3. Define las dimensiones:
+Crea una variable llamada altura y asigna el valor 5.
+Crea una variable llamada longitud y asigna el valor 10.
+4. Calcula la suma:
+Crea una variable llamada suma y asigna el valor de altura + longitud.
+5. Imprime los resultados:
+Muestra todas las variables por consola. */
+
+//1
+let miSuperheroeFavorito = 'Hulk'
+//2
+let numeroFavorito = 50
+//3
+let altura = 5
+let longitud = 10
+//4
+let suma = altura + longitud
+//5
+console.log('Mi superhéroe favorito:', miSuperheroeFavorito)
+console.log('Mi número favorito:', numeroFavorito)
+console.log('Altura:', altura)
+console.log('Longitud:', longitud)
+console.log('Suma de altura y longitud:', suma)
---
+++ ejercicio3.js
@@ -0,0 +1,11 @@
+/* En este ejercicio podéis mostrar directamente los resultados por consola o almacenarlos en variables (esto lo podéis hacer en todos los demás, como os resulte más claro). */
+//3.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+console.log('Multiplicación de 10 por 5 = ' + 10 * 5)
+//3.2 Divide 10 por 2 y muestra el resultado en un console.
+console.log('División de 10 entre 2 = ' + 10 / 2)
+//3.3 Muestra mediante un console el resto de dividir 15 por 9.
+console.log('Resto de dividir 15 entre 9 = ' + (15 % 9))
+//3.4 Usa el correcto operador de asignación que resultará en o = 15, teniendo dos variables p = 10 y j = 5.
+console.log('o = p + j = 15 donde p = 10 y j = 5')
+//3.5 Usa el correcto operador de asignación que resultará en i = 50, teniendo dos variables c = 10 y m = 5.
+console.log('i = c * m = 50 donde c = 10 y m = 5')
