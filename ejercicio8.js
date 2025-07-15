867450d3d3dfea72bd0cb955251355e161a4ac02
r-valero <r.valero0600@gmail.com>
2025-07-15 19:26:21 +0200

    mas ejercicios

---
+++ ejercicio8.js
@@ -0,0 +1,21 @@
+/* Ejercicio 8
+Buscar la palabra más larga: Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+Puedes usar este array para probar tu función: */
+const avengers = [
+  'Hulk',
+  'Thor',
+  'Iron Man',
+  'Captain A.',
+  'Spiderman',
+  'Captain M.'
+]
+function findLongestWord(stringList) {
+  let longestWord = ''
+  for (let i = 0; i < stringList.length; i++) {
+    if (stringList[i].length > longestWord.length) {
+      longestWord = stringList[i]
+    }
+  }
+  console.log(longestWord)
+}
+findLongestWord(avengers)
---
+++ ejercicio9.js
@@ -0,0 +1,13 @@
+/* Ejercicio 9
+Calcular una suma puede ser tan simple como iterar sobre un array y sumar cada uno de los elementos.
+Completa la función denominada sumNumbers que toma un array de números como argumento y devuelve la suma de todos los números del array.
+Puedes usar este array para probar tu función: */
+const numbers = [1, 2, 3, 5, 45, 37, 58]
+function sumNumbers(numberList) {
+  let total = 0
+  for (let i = 0; i < numberList.length; i++) {
+    total += numberList[i]
+  }
+  console.log(total)
+}
+sumNumbers(numbers)
